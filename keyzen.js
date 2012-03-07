297191a24a432bd9d5e6ea4d79d1475d31e8cab0
Josh Kaufman <josh@eurekaoverdrive.com>
2012-03-07 16:23:04 -0700

    Update keyzen.js

--- keyzen.js
+++ keyzen.js
@@ -1,7 +1,6 @@
 
-
 var data = {};
-data.chars = " jfkdlsahgyturieowpqbnvmcxz6758493021`-=[]\\;',./ABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%^&*()_+{}|:\"<>?";
+data.chars = " nesirahdjglpufywoqbkvmcxz1234567890'\",.!?:;/@$%&#*()_ABCDEFGHIJKLMNOPQRSTUVWXYZ~+-={}|^<>`[]\\";
 data.consecutive = 10;
 data.word_length = 7;
 
